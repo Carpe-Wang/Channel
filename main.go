b9822693ac91892e49d69e491067e9d6438a84fd
Carpe-Wang <1592622761@qq.com>
2022-03-27 12:37:10 +0800

    go socket demo

--- main.go
+++ main.go
@@ -1,70 +1,55 @@
 package main
 
-//
-//import "fmt"
-//
-////interface
-//
-//type Human struct {
-//	name  string
-//	age   int
-//	phone string
-//}
-//type Student struct {
-//	Human  //匿名字段Human
-//	school string
-//	loan   float32
-//}
-//type Employee struct {
-//	Human   //匿名字段Human
-//	company string
-//	money   float32
-//}
-//
-////Human对象实现Sayhi方法
-//func (h *Human) SayHi() {
-//	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
-//}
-//
-//// Human对象实现Sing方法
-//func (h *Human) Sing(lyrics string) {
-//	fmt.Println("La la, la la la, la la la la la...", lyrics)
-//}
-//
-////Human对象实现Guzzle方法
-//func (h *Human) Guzzle(beerStein string) {
-//	fmt.Println("Guzzle Guzzle Guzzle...", beerStein)
-//}
-//
-//// Employee重载Human的Sayhi方法
-//func (e *Employee) SayHi() {
-//	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
-//		e.company, e.phone) //此句可以分成多行
-//}
-//
-////Student实现BorrowMoney方法
-//func (s *Student) BorrowMoney(amount float32) {
-//	s.loan += amount // (again and again and...)
-//}
-//
-////Employee实现SpendSalary方法
-//func (e *Employee) SpendSalary(amount float32) {
-//	e.money -= amount // More vodka please!!! Get me through the day!
-//}
-//
-//// 定义interface
-//type Men interface {
-//	SayHi()
-//	Sing(lyrics string)
-//	Guzzle(beerStein string)
-//}
-//type YoungChap interface {
-//	SayHi()
-//	Sing(song string)
-//	BorrowMoney(amount float32)
-//}
-//type ElderlyGent interface {
-//	SayHi()
-//	Sing(song string)
-//	SpendSalary(amount float32)
-//}
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	//监听
+	listen, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer listen.Close()
+	//阻塞等待用户连接
+
+	for { //接受多个用户
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		////接受用户请求
+		//buf := make([]byte, 1024) //设置1024缓冲区
+		//n, err1 := conn.Read(buf)
+		//if err1 != nil {
+		//	fmt.Println(err1)
+		//	continue
+		//}
+		//fmt.Printf("buf:", string(buf[:n])) //读了多少
+		//defer conn.Close()
+
+		go HandleConn(conn)
+	}
+	//接受用户请求
+}
+
+func HandleConn(conn net.Conn) {
+	//获取网络信息
+	add := conn.RemoteAddr().String()
+	add = add
+	fmt.Println("Connection is successful ")
+	buf := make([]byte, 2048)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("has err:", err)
+		return
+	}
+	fmt.Println(string(buf[:n]))
+
+	//把数据转化为大写，并发送给用户
+	conn.Write([]byte(strings.ToUpper(string(buf[:n]))))
+}
